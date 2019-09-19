1a236155eb7bfb16f0a2808849408353374a331c
Thomas Alrek <thomas@alrek.no>
2019-09-19 20:36:48 +0200

    Remove indicators

--- components/carousel.php
+++ components/carousel.php
@@ -17,15 +17,6 @@ $slides = get_block_content_list($carouselSettings);
 ?>
 <?= set_edit_btn($carouselSettings) ?>
 <div id="<?= $blockhash ?>_carousel" class="carousel slide pb-3" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <? if (isset($_mode) && !count($slides)) { ?>
-      <li data-target="#<?= $blockhash ?>_carousel_indicators" data-slide-to="0" class="active"></li>
-    <? } else { ?>
-      <? foreach ($sides as $i => $slide) { ?>
-        <li data-target="#<?= $blockhash ?>_carousel_indicators" data-slide-to="<?= $i ?>" class="<?= !$i ? 'active' : '' ?>"></li>
-      <? } ?>
-    <? } ?>
-  </ol>
   <div class="carousel-inner">
     <? if (isset($_mode) && !count($slides)) { ?>
       <div class="carousel-item active">
@@ -39,12 +30,4 @@ $slides = get_block_content_list($carouselSettings);
       <? } ?>
     <? } ?>
   </div>
-  <a class="carousel-control-prev" href="#<?= $blockhash ?>_carousel_indicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Forrige</span>
-  </a>
-  <a class="carousel-control-next" href="#<?= $blockhash ?>_carousel_indicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Neste</span>
-  </a>
 </div>
