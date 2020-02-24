a8a42fb748b58efb1c437e065e1869f9654ddf8a
Thomas Alrek <thomas@alrek.no>
2020-02-24 01:24:44 +0100

    Add temporary scanner

---
+++ public/scan/precache-manifest.07e18a762125ff63fe419a5343581d42.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "debf2031e7710b7a95af",
+    "url": "/css/about.88f51862.css"
+  },
+  {
+    "revision": "c411643f85603cf36991",
+    "url": "/css/chunk-vendors.61f4bc4c.css"
+  },
+  {
+    "revision": "092d262043b6fc512efb8fd799983b00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "debf2031e7710b7a95af",
+    "url": "/js/about.e92cb1e1.js"
+  },
+  {
+    "revision": "23d22ec979a209851d96",
+    "url": "/js/app.c2dd81a5.js"
+  },
+  {
+    "revision": "c411643f85603cf36991",
+    "url": "/js/chunk-vendors.e2ee854e.js"
+  },
+  {
+    "revision": "b561d86700dae83d3275417c693edee9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ public/scan/precache-manifest.f06e9e3e1088576107e0196d789d1663.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "debf2031e7710b7a95af",
+    "url": "/scan/css/about.88f51862.css"
+  },
+  {
+    "revision": "c411643f85603cf36991",
+    "url": "/scan/css/chunk-vendors.61f4bc4c.css"
+  },
+  {
+    "revision": "73f12794d0d9cde39ab7380081fcd5c6",
+    "url": "/scan/index.html"
+  },
+  {
+    "revision": "debf2031e7710b7a95af",
+    "url": "/scan/js/about.e92cb1e1.js"
+  },
+  {
+    "revision": "48fe73fd7a44e8300cb4",
+    "url": "/scan/js/app.c6893dad.js"
+  },
+  {
+    "revision": "c411643f85603cf36991",
+    "url": "/scan/js/chunk-vendors.e2ee854e.js"
+  },
+  {
+    "revision": "b561d86700dae83d3275417c693edee9",
+    "url": "/scan/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/scan/robots.txt"
+  }
+]);
