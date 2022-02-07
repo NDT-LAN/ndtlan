dd07cf4415bea94ceec9d3f26937d6a8a49153ef
Thomas Alrek <thomas@alrek.no>
2022-02-07 10:24:11 +0100

    Add covid seating type

--- blocks/seating_explanation.php
+++ blocks/seating_explanation.php
@@ -1,7 +1,8 @@
 <?php
-  use Helpers\NDT;
 
-  $user = NDT::currentUser();
+use Helpers\NDT;
+
+$user = NDT::currentUser();
 ?>
 <div class="container bg-dark">
   <ul class="list-inline pt-3 pl-3 pr-3">
@@ -19,17 +20,22 @@
     <li class="list-inline-item">
       <button class="ndt-seat btn ndt-seat--taken">&nbsp;</button> Opptatt
     </li>
-    <li class="list-inline-item">
+    <? if (get_setting('covid_seating')) { ?>
+      <li class="list-inline-item">
+        <button class="ndt-seat btn ndt-seat--info ndt-seat--covid">&nbsp;</button> Utilgjengelig pga. smittevernstiltak
+      </li>
+    <? } ?>
+    <!-- <li class="list-inline-item">
       <button class="ndt-seat btn ndt-seat--info">&nbsp;</button> Inngang / Vakt
-    </li>
-    <li class="list-inline-item">
+    </li> -->
+    <!-- <li class="list-inline-item">
       <button class="ndt-seat btn ndt-seat--activity">&nbsp;</button> Aktivitet
-    </li>
-    <li class="list-inline-item">
+    </li> -->
+    <!-- <li class="list-inline-item">
       <button class="ndt-seat btn ndt-seat--kiosk">&nbsp;</button> Kiosk
-    </li>
-    <li class="list-inline-item">
+    </li> -->
+    <!-- <li class="list-inline-item">
       <button class="ndt-seat btn ndt-seat--scene">&nbsp;</button> Scene
-    </li>
+    </li> -->
   </ul>
 </div>
